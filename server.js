02e1551edcc59aac449f117e7f78a0dbc4f3cd73
Kristopher Kleva <kriskleva@gmail.com>
2014-08-11 14:09:52 -0400

    add support for post

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-var http = require('http')
+
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+var argo = require('argo');
+argo()
+  .get('^/dogs$', function(handle) {
+    handle('request', function(env, next) {
+      env.response.statusCode = 200;
+      env.response.body = { dogs: ['Alfred', 'Rover', 'Dino'] };
+      next(env);
+    });
+  }).listen(port);
