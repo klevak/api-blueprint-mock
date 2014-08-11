a2dffcaabd2a93f12763025122abeb51eb775915
Kristopher Kleva <kriskleva@gmail.com>
2014-08-11 14:22:07 -0400

    xml

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ var argo = require('argo');
 
 argo()
   .post('^/dogs$', function(handle) {
-    handle('request', function(env, next) {
+    handle('request', function(env, next) { 
       env.response.statusCode = 200;
-      env.response.body = 'hello';
+      env.response.body = '<xml></xml>';
+      env.response.header('Content-Type','text/xml');
       next(env);
     });
   }).listen(port);
