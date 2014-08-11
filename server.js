a705ffcaf873761c144eeab10e8e1768f9a09f65
Kristopher Kleva <kriskleva@gmail.com>
2014-08-11 14:20:34 -0400

    not there

--- server.js
+++ server.js
@@ -2,29 +2,6 @@
 var port = process.env.PORT || 1337;
 var argo = require('argo');
 
-var xml = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
-   <soapenv:Body>
-      <publishMarketingEventResponse xmlns="http://services.llbean.com/marketing/event/publication/v1_0">
-         <responseSummary>
-            <requestReceivedTimestamp>2014-07-20T12:00:00.100</requestReceivedTimestamp>
-            <processingCompleteTimestamp>2014-07-20T12:00:00.150</processingCompleteTimestamp>
-            <processingTimeMilliseconds>50</processingTimeMilliseconds>
-            <requestorAuditId>2</requestorAuditId>
-            <serviceAuditId>3</serviceAuditId>
-            <unitOfWorkId>4</unitOfWorkId>
-            <responseCode>00</responseCode>
-            <responseDetailList/>
-            <registryConsumerId>0</registryConsumerId>
-            <registryContextId>1</registryContextId>
-            <originatingRequestor>aRequestor</originatingRequestor>
-            <originatingUserName>aUser</originatingUserName>
-            <originatingTimestamp>2014-07-20T12:00:00.000</originatingTimestamp>
-         </responseSummary>
-         <eventGUID>someGUIDsomeGUIDsomeGUIDsomeGUID</eventGUID>
-      </publishMarketingEventResponse>
-   </soapenv:Body>
-</soapenv:Envelope>
-';
 
 
 argo()
