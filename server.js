464bdd37e549137962c87a7b6c5821a713faff86
Kristopher Kleva <kriskleva@gmail.com>
2014-08-11 14:10:26 -0400

    switch to post

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var port = process.env.PORT || 1337;
 var argo = require('argo');
 argo()
-  .get('^/dogs$', function(handle) {
+  .post('^/dogs$', function(handle) {
     handle('request', function(env, next) {
       env.response.statusCode = 200;
       env.response.body = { dogs: ['Alfred', 'Rover', 'Dino'] };
